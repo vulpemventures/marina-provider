3446b5267fe2b8cb920b77752120475497a4f40a
Marco Argentieri <3596602+tiero@users.noreply.github.com>
2021-03-28 01:16:49 +0100

    MarinaProvider interface

---
+++ index.ts
@@ -0,0 +1,34 @@
+export interface AddressInterface {
+  confidentialAddress: string;
+  blindingPrivateKey: string;
+  derivationPath?: string;
+}
+
+
+export interface MarinaProvider {
+  enable(): Promise<void>;
+
+  disable(): Promise<void>;
+
+  isEnabled(): Promise<boolean>;
+
+  setAccount(account: number): Promise<void>;
+
+  getNetwork(): Promise<'liquid' | 'regtest'>;
+
+  getAddresses(): Promise<AddressInterface[]>;
+
+  getNextAddress(): Promise<AddressInterface>;
+
+  getNextChangeAddress(): Promise<AddressInterface>;
+
+  sendTransaction(
+    recipientAddress: string,
+    amountInSatoshis: number,
+    assetHash: string
+  ): Promise<string>;
+
+  blindTransaction(psetBase64: string): Promise<string>;
+
+  signTransaction(psetBase64: string): Promise<string>;
+}
