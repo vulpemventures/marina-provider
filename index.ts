4ffdc944cec99c7e0460b1bd375fe01da5a11c63
louisinger <louis@vulpem.com>
2021-07-05 11:26:39 +0200

    add coins, balances, transactions and marina.on

--- index.ts
+++ index.ts
@@ -9,6 +9,39 @@ export interface SignedMessage {
   address: NativeSegwitAddress;
 }
 
+export enum TxStatusEnum {
+  Confirmed = 1,
+  Pending = 0,
+}
+
+export interface Transaction {
+  txId: string;
+  status: TxStatusEnum;
+  fee: number;
+  transfers: Array<{ asset: string; amount: number; }>;
+  explorerURL: string;
+  blocktimeMs: number;
+}
+
+export interface Utxo {
+  txid: string;
+  vout: number;
+  asset?: string;
+  value?: number;
+}
+
+export interface Balance {
+  asset: {
+    assetHash: string;
+    ticker?: string;
+    name?: string;
+    precision: number;
+  },
+  amount: number;
+}
+
+export type MarinaEventType = 'new_utxo' | 'new_tx' | 'spent_utxo';
+
 export type TransactionHex = string;
 export type PsetBase64 = string;
 export type SignatureBase64 = string;
@@ -43,4 +76,12 @@ export interface MarinaProvider {
   signTransaction(pset: PsetBase64): Promise<PsetBase64>;
 
   signMessage(message: string): Promise<SignedMessage>;
+
+  getCoins(): Promise<Utxo[]>;
+
+  getTransactions(): Promise<Transaction[]>;
+
+  getBalances(): Promise<Balance[]>;
+
+  on(type: MarinaEventType, callback: (payload: any) => void): void;
 }
