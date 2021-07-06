05cdfaee34db34ce0640375cb4cefdc73ef566a4
louisinger <louis@vulpem.com>
2021-07-06 10:24:05 +0200

    typescript declarations

--- index.d.ts
+++ index.d.ts
@@ -37,7 +37,7 @@ export interface Balance {
     };
     amount: number;
 }
-export declare type MarinaEventType = 'NEW_UTXO' | 'NEW_TX' | 'SPENT_UTXO' | 'ENABLED' | 'DISABLED';
+export declare type MarinaEventType = 'NEW_UTXO' | 'NEW_TX' | 'SPENT_UTXO' | 'ENABLED' | 'DISABLED' | 'NETWORK';
 export declare type TransactionHex = string;
 export declare type PsetBase64 = string;
 export declare type SignatureBase64 = string;
