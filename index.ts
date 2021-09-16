f0085f7e7bb3068e4902cd81accede845d6dd5e0
louisinger <louis@vulpem.com>
2021-09-16 14:30:03 +0200

    add detectProvider function

--- index.ts
+++ index.ts
@@ -94,3 +94,54 @@ export interface MarinaProvider {
 
   getFeeAssets(): Promise<string[]>;
 }
+
+
+
+/**
+ * listen with timeout the `providerName#initialized` event
+ * @param provider the name of the provider to detect "marina" for window.marina
+ * @param timeout configurable timeout, default is 3000 (expressed in milliseconds)
+ */
+export async function detectProvider<T = MarinaProvider>(provider: string = 'marina', timeout: number = 3000): Promise<T> {
+  let handled = false;
+  let windowObject = window as any;
+
+  return new Promise<T>((resolve, reject) => {
+
+    if (windowObject[provider]) {
+      handleProvider();
+    } else {
+      window.addEventListener(
+        `${provider}#initialized`,
+        handleProvider,
+        { once: true },
+      );
+
+      setTimeout(() => {
+        handleProvider();
+      }, timeout);
+    }
+
+    function handleProvider() {
+      if (handled) return;
+      handled = true;
+
+      window.removeEventListener(`${provider}#initialized`, handleProvider);
+      if (typeof windowObject[provider] !== 'undefined') {
+        resolve(windowObject[provider]);
+        return;
+      }
+
+      reject(new DetectProviderTimeoutError(provider, timeout));
+    }
+  })
+  }
+
+export class DetectProviderTimeoutError extends Error {
+  provider: string;
+  timeout: number;
+
+  constructor(provider: string, timeout: number) {
+    super(`detectProviderTimeout: detection of ${provider} timeout (${timeout} ms)`)
+  }
+}
